7e620498023bffe57e431dcad1c42df0f2c24b62
Paras Chandra <paraschandra30@gmail.com>
2024-12-10 15:49:57 +0530

    lecture 11

--- MySQL/lec_11_window_func.sql
+++ MySQL/lec_11_window_func.sql
@@ -1,2 +1,45 @@
 -- WINDOW FUNCTION
 
+-- GROUP BY fails in the cases where we don't want to roll up the table but still want to perform aggregate operations over some partions
+-- here comes the window functions
+
+SELECT gender, AVG(salary) OVER(PARTITION BY gender)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+SELECT dem.first_name, dem.last_name, gender, 
+SUM(salary) OVER(PARTITION BY gender)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+
+-- Rolling total
+SELECT dem.first_name, dem.last_name, gender, sal.salary,
+SUM(sal.salary) OVER(PARTITION BY gender ORDER BY dem.employee_id) AS rolling_total
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+-- Row Number
+SELECT dem.employee_id, dem.first_name, dem.last_name, gender, sal.salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+-- RANK & Dense Rank
+SELECT dem.employee_id, dem.first_name, dem.last_name, gender, sal.salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+SELECT dem.employee_id, dem.first_name, dem.last_name, gender, sal.salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num, #positionally assign rank
+DENSE_RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS dense_rank_num #numerically assign rank
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
